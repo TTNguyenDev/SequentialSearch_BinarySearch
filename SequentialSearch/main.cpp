56ed7c86bedc1c972199bda9985d494301f20f49
TTNguyen <nttriet9a4@gmail.com>
2018-10-16 10:54:13 +0700

    First Version

--- SequentialSearch/main.cpp
+++ SequentialSearch/main.cpp
@@ -1,15 +1,33 @@
-//
-//  main.cpp
-//  SequentialSearch
-//
-//  Created by TT Nguyen on 10/16/18.
-//  Copyright © 2018 TT Nguyen. All rights reserved.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int sequentialSearch(int a[], int n, int x) {
+    for (int i = 0; i < n; i++)
+        if (a[i] == x)
+            return i;
+        return -1;
+}
+
+int binarySearch(int a[], int n, int x) {
+    int left = 0;
+    int right = n - 1;
+    
+    while (left <= right) {
+        int mid = (left + right)/2;
+        if (a[mid] == x)
+            return mid;
+        if (a[mid] < x)
+            left = mid + 1;
+        else
+            right = mid + 1;
+    }
+    return -1;
+}
+
+int main() {
+    int a[10] = { 5, 7, 1, 11, 13, 15, 16, 19, 25, 0};
+    cout << "se:" << sequentialSearch(a, 10, 13) << endl;
+    int ab = binarySearch(a, 10, 0);
+    cout << "bi:" << ab << endl;
 }
